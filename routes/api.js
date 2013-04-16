5767cbc805778636a9a08c32c1d8c474a59f8b11
Bart Lantz <underhilllabs@gmail.com>
2013-04-16 00:42:18 -0400

    updated votes are now saved to mongoDB

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,13 @@
 // initialize our faux database
 var mongoose = require('mongoose');
 var CaptainModel = mongoose.model('Captain');
+var captainsArr = [];
 var data = {};
-//var data = {
-//  "captains": [
-//    {"name": "Jean-Luc Picard","image": "img/picard.jpg", "source": "Star Trek: TNG", "votes": 2, index: 0}
-//    , {"name": "James Tiberius Kirk","image": "img/kirk.jpg", "source": "Star Trek: TOS", "votes": 0, index: 1}
-//    , {"name": "Kathryn Janeway", "image": "img/janeway.jpg", "source": "Star Trek Voyager ", "votes": 0, index: 2}
-//    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 1, index: 3}
-//    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 4, index: 4}
-//    , {"name": "Worf, Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0, index: 5}
-//    , {"name": "Benjamin Sisko","image": "img/sisko.jpg", "source": "Star Trek: DSN", "votes": 0, index: 6}
-//    , {"name": "Han Solo","image": "img/solo.jpg", "source": "Star Wars", "votes": 0, index: 7}
-//  ]
-//};
 
 // GET
-
 exports.captains = function (req, res) {
-  var captainsArr = [];
+  // need to reset array each time this function is called.
+  captainsArr = [];
   CaptainModel.find({}, function(err, docs) {
     for (i = 0; i < docs.length; i++) {
       captainsArr.push({
@@ -42,7 +31,7 @@ exports.captain = function (req, res) {
   var id = req.params.id;
   if (id >= 0 && id < data.captains.length) {
     res.json({
-      captain: data.captains[id]
+      captain: captainsArr[id]
     });
   } else {
     res.json(false);
@@ -56,7 +45,7 @@ exports.addCaptain = function(req, res) {
     image: req.body.cptUrl,
     source: req.body.cptSource,
     votes: req.body.votes,
-    index: req.body.index
+    index: req.body.idx
   });
   captain.save(function (err) {
     if (!err) {
@@ -65,6 +54,7 @@ exports.addCaptain = function(req, res) {
       return console.log(err);
     }
   });
+  captainsArr[idx] = captain;
   //captain = {name: req.body.cptName, image: req.body.cptUrl, source: req.body.cptSource, votes: req.body.votes, index: req.body.index};
   //data.captains.push(captain);
   //data.captains.push(req.body);
@@ -74,9 +64,17 @@ exports.addCaptain = function(req, res) {
 // PUT
 exports.updateCaptain = function(req, res) {
   var id = req.params.id;
-  if (id >= 0 && id < data.captains.length) {
-    //captain = {name: req.body.cptName, image: req.body.cptUrl, source: req.body.cptSource, votes: req.body.votes, index: req.body.index};
-    data.captains[id] = req.body;
+  if (id >= 0 && id < captainsArr.length) {
+    captainsArr[id] = req.body;
+    CaptainModel.findOne({'index': id}, function (err, cptn) {
+      if (err) console.log("no findOne for you! " + err);
+      cptn.votes = req.body.votes;
+      console.log(cptn.name + " has " + cptn.votes + " votes!");
+      cptn.save(function (err) {
+        if (err) console.log(err);
+        res.send(cptn);
+      });
+    });
     res.json(true);
   } else {
     res.json(false);
